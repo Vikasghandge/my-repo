06838912742f87c765adefa3035e9de5c2e1abd7
vikasghandge <ghandgevikas804@gmail.com>
2024-08-15 12:01:13 +0530

    --- Auto Git Commit ---

--- e2_script/ec2.tf
+++
@@ -1,4 +0,0 @@
-resource "aws" "ec2_script" {
-    region = "ap-south-1"
-  
-}
--- e2_script/privider.tf
+++
@@ -1,9 +0,0 @@
-resource "aws" "ec2" {
-    ami =  ""
-    instance_type = "t2.micro"
-    
-  
-}
-tags = {
-    name = {testing sever}
-}
