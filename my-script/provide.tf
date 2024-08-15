0efcc3e7664c5ae6461193f1ba279c290be8ea15
vikasghandge <ghandgevikas804@gmail.com>
2024-08-15 12:17:03 +0530

    --- Auto Git Commit ---

--- my-script/provide.tf
+++ my-script/provide.tf
@@ -5,7 +5,7 @@ provider "aws" {
 # aws resources 
 
 resource "aws_instance" "my-test" {
-    ami = ""
+    ami = "ami-0ad21ae1d0696ad58"
     instance_type = "t2.mirco"
     key_name = "devops-key"
   
