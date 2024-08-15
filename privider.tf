08f8390272bf1f49489e2d962bf9282c08ca7f45
vikasghandge <ghandgevikas804@gmail.com>
2024-08-15 11:57:41 +0530

    --- Auto Git Commit ---

--- privider.tf
+++ privider.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "my_ec2_instance" {
     ami =  "ami-0ad21ae1d0696ad58"
     instance_type = "t2.micro"
-    key_name = "devops-key"
+    keykey_name =  = "devops-key"
 }
-
+tags =
 
   
